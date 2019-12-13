83aed7bb0422b11d9b3aada4dd0e2d4460eaaf3f
Prabhakar, Piyush (Cognizant) <PIYUSH.PRABHAKAR@cognizant.com>
2019-12-13 11:26:49 +0530

    Initial Commit

---
+++ Rideshare_iosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rideshare_iosApp
+//
+//  Created by Prabhakar, Piyush (Cognizant) on 13/12/2019.
+//  Copyright © 2019 Prabhakar, Piyush (Cognizant). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
